9053c4beaa93bb05f3529a5bb93c231b730e1681
soyidwahyud <soyidwahyu4@gmail.com>
2019-05-11 14:35:04 +0700

    upload to deploy to heroku

--- server.js
+++ server.js
@@ -3,10 +3,10 @@ const express = require('express');
 const path = require('path');
 const app = express();
 
-app.use(express.static(__dirname + '/herojihan-app'));
+app.use(express.static(__dirname + '/app-herokuu08'));
 
 app.all('*', (req, res) => {
-  res.status(200).sendFile(__dirname + '/herojihan-app/index.html');
+  res.status(200).sendFile(__dirname + '/app-herokuu08/index.html');
 });
 
 app.listen(process.env.PORT || 8080);
